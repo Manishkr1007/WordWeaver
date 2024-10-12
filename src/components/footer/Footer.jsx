704e26c88c8c0464719e983566ffb2521ee3bcfa
Manish kumar <113241137+Manishkr1007@users.noreply.github.com>
2024-10-12 15:06:56 +0530

    Revert "Added Social Media Section"

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import Logo from "../Logo";
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import React from 'react'
+import {Link} from "react-router-dom"
+import Logo from "../Logo"
+
 
 function Footer() {
     return (
@@ -13,7 +13,7 @@ function Footer() {
                             <div className="mb-4 inline-flex items-center">
                                 <Logo width="100px" />
                             </div>
-                            <h1 className="mb-10 text-1xl font-bold text-white">WordWeaver</h1>
+                            <h1 className='mb-10 text-1xl font-bold text-white'>WordWeaver</h1>
                             <div>
                                 <p className="text-sm md:text-center text-white">
                                     &copy; Copyright 2024. All Rights Reserved by WordWeaver.
@@ -21,77 +21,72 @@ function Footer() {
                             </div>
                         </div>
                     </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-7/12 md:items-start">
-                        <div className="flex flex-wrap justify-between">
+                    <div className="w-full p-6  md:w-1/2 lg:w-7/12 md:items-start">
+                        <div className="flex flex-wrap  justify-between">
+                           
                             <div className="w-full p-6 md:p-0 md:w-1/2 lg:w-2/12">
-                                <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-white text-center md:text-left">
-                                    Support
-                                </h3>
-                                <ul className="flex flex-col items-center md:flex-none md:items-start">
-                                    <li className="mb-4">
-                                        <Link className="text-base font-medium text-white hover:text-gray-700" to="/">
-                                            Account
-                                        </Link>
-                                    </li>
-                                    <li className="mb-4">
-                                        <Link className="text-base font-medium text-white hover:text-gray-700" to="/">
-                                            Help
-                                        </Link>
-                                    </li>
-                                    <li className="mb-4">
-                                        <Link className="text-base font-medium text-white hover:text-gray-700" to="/contact">
-                                            Contact Us
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="w-full p-6 md:p-0 md:w-1/2 lg:w-3/12">
-                                <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-white text-center md:text-left">
-                                    Legals
-                                </h3>
-                                <ul className="flex flex-col items-center md:flex-none md:items-start">
-                                    <li className="mb-4">
-                                        <Link className="text-base font-medium text-white hover:text-gray-700" to="/">
-                                            Terms &amp; Conditions
-                                        </Link>
-                                    </li>
-                                    <li className="mb-4">
-                                        <Link className="text-base font-medium text-white hover:text-gray-700" to="/">
-                                            Privacy Policy
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                            {/* Social Media Section */}
-                            <div className="w-full p-6 md:p-0 md:w-1/2 lg:w-3/12">
-                                <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-white text-center md:text-left">
-                                    Follow Us
-                                </h3>
-                                <ul className="flex space-x-6 justify-center md:justify-start">
-                                    <li>
-                                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                                            <FaFacebook className="text-white hover:text-gray-500" size={24} />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                                            <FaTwitter className="text-white hover:text-gray-500" size={24} />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                                            <FaInstagram className="text-white hover:text-gray-500" size={24} />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                                            <FaLinkedin className="text-white hover:text-gray-500" size={24} />
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+                        <div className="w-full md:w-1/2 lg:w-1/4 mb-8 md:mb-0">
+                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white  text-center md:text-left">
+                                Support
+                            </h3>
+                            <ul className='flex flex-col items-center md:flex-none md:items-start'>
+                                <li className="mb-4">
+                                    <Link
+                                        className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                                        to="/"
+                                    >
+                                        Account
+                                    </Link>
+                                </li>
+                                <li className="mb-4">
+                                    <Link
+                                        className=" text-base font-medium text-white hover:text-gray-700"
+                                        to="/"
+                                    >
+                                        Help
+                                    </Link>
+                                </li>
+                                <li className="mb-4">
+                                    <Link
+                                        className=" text-base font-medium text-white hover:text-gray-700"
+                                        to="/contact"
+                                    >
+                                        Contact Us
+                                    </Link>
+                                </li>
+                               
+                            </ul>
                         </div>
                     </div>
+                    <div className="w-full md:p-0 p-6 md:w-1/2 lg:w-3/12">
+                        <div className="h-full">
+                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white text-center md:text-left">
+                                Legals
+                            </h3>
+                            <ul className='flex flex-col  items-center md:flex-none md:items-start'>
+                                <li className="mb-4">
+                                    <Link
+                                        className=" text-base font-medium text-white hover:text-gray-700"
+                                        to="/"
+                                    >
+                                        Terms &amp; Conditions
+                                    </Link>
+                                </li>
+                                <li className="mb-4">
+                                    <Link
+                                        className=" text-base font-medium text-white hover:text-gray-700"
+                                        to="/"
+                                    >
+                                        Privacy Policy
+                                    </Link>
+                                </li>
+                           
+                            </ul>
+                        </div>
+                    </div>
+
+                        </div>
+                    </div>--
                 </div>
             </div>
         </section>
