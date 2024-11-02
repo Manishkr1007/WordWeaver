75277611099daf18e5a9c14cb053083fbbc9d0ec
japsare ayushi <japsareayushi@gmail.com>
2024-11-03 01:49:24 +0530

    Feat: backend setup

--- frontend/src/components/BloggersProfile.jsx
+++ frontend/src/components/BloggersProfile.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const BloggersProfile = () => {
     const [drafts, setDrafts] = useState([]);
--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -1,6 +1,7 @@
-import  {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link, useNavigate, useParams} from "react-router-dom"
 
+import appwriteService from "../appwrite/config"
 import Button from "../components/Button"
 import Container from "../components/container/Container"
 import parse from "html-react-parser"
